34178424af9e2ee4da54752538e491b4645c50fe
Firdavs Bagirov <firdavsbagirov@gmail.com>
2020-10-30 17:14:32 +0500

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Firdavs Bagirov on 10/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
